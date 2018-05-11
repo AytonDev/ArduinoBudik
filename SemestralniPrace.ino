995b4abab642c06e39ad719c8e8465061782b2a7
Lukáš Frajt <lukasfrajt@Lukass-MacBook-Pro-2.local>
2018-05-11 23:34:02 +0200

    EEPROM setup, check alarms, RTC update

--- SemestralniPrace.ino
+++ SemestralniPrace.ino
@@ -1,6 +1,6 @@
 
 /*LIBRARIES*/
-
+#include <EEPROM.h>
 /*--I2C*/
 #include <Wire.h>
 /*--RTCmodule*/
@@ -20,6 +20,8 @@
 #define CLK_DISPLAY 2
 #define DIO_DISPLAY 3
 
+
+#define ALARM_COUNT 5
 
 
 struct T_Time {
@@ -42,7 +44,11 @@ int8_t TimeDisp[] = {0x00,0x00,0x00,0x00};
 TM1637 tm1637(CLK_DISPLAY,DIO_DISPLAY);
 
 
+int addresses[] ={0,3,6,9,12}; 
+T_Alarm alarms[5];
 
+int a = 0;
+int value;
 
 void setup() {
   // put your setup code here, to run once:
@@ -56,30 +62,99 @@ void setup() {
   InitRTC();
   /**SETUP DISPLAY**/
   InitDisplay();
+
+ //DefaultAlarms();
+  clearEEPROM();
  
 
 }
 
 void loop() {
 
-  m_Time = GetTime();
+  //m_Time = GetTime();
+ 
 
-  Timer1.initialize(500000);//timing for 500ms
-  Timer1.attachInterrupt(CheckAlarms);//declare the interrupt serve routine:TimingISR
+  for (int i = 0 ; i < 20 ; i++) {
+    byte value = EEPROM.read(i);                //read EEPROM data at address i
+    if(value != 0)                              //skip "empty" addresses
+    {
+                   //take care of the offset
+      Serial.print(i);
+      Serial.print(" : ");
+      Serial.println(value);
+       delay(500);
+    }
+  }
+  delay(1000);
+  
+
+  DefaultAlarms();
  
   // put your main code here, to run repeatedly:
 }
 
-
-void SaveAlarm() {
-  
+//Clears all data from EEPROM
+void clearEEPROM()
+{
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    if(EEPROM.read(i) != 0)                     //skip already "empty" addresses
+    {
+      EEPROM.write(i, 0);                       //write 0 to address i
+    }
+  }
+  Serial.println("EEPROM erased");
+                                 //reset address counter
 }
 
+
+//SET FIVE DEFAULT ALARMS
+void DefaultAlarms() {
+  for(int i =0; i< ALARM_COUNT; ++i) {
+    alarms[i].minutes = 30+i;
+    alarms[i].hours = 7+i;
+    alarms[i].active = true;
+  }
+
+  for(int i = 0; i <ALARM_COUNT; i++) {
+    EEPROM.write(addresses[i], alarms[i].hours);
+    EEPROM.write(addresses[i]+1, alarms[i].minutes);
+    EEPROM.write(addresses[i]+2, alarms[i].active);
+   }
+}
+
+/*
+ *Save Alarm to the EEPROM
+ */
+void SaveAlarm(int position) {
+ 
+  EEPROM.write(addresses[position], alarms[position].hours);
+  EEPROM.write(addresses[position]+1, alarms[position].minutes);
+  EEPROM.write(addresses[position]+2, alarms[position].active);
+}
+
+/**
+ * Load all alarms from EEPROM
+ */
 void LoadAlarms() {
   
+  for(int i =0; i< ALARM_COUNT; ++i) {
+    alarms[i].minutes =  EEPROM.read(addresses[i]);
+    alarms[i].hours = EEPROM.read(addresses[i]+1);
+    alarms[i].active = EEPROM.read(addresses[i]+2);
+  }
 }
 
+
+/* 
+ *  If current time is equal with one of the active alarm -> beep()
+ */
 void CheckAlarms() {
+
+  for(int i =0; i< ALARM_COUNT; ++i) {
+    if( alarms[i].active && (alarms[i].hours == m_Time.hours) && (alarms[i].minutes == m_Time.minutes) {
+      Beep();
+    }
+  }
   
 }
 
